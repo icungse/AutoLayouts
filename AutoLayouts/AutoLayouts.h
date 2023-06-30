1fe1dbfe7889667359f62bd5d2a30facfedd523a
Icung <icung@Icungs-MacBook-Air.local>
2023-06-30 10:51:47 +0700

    initial commit

---
+++ AutoLayouts/AutoLayouts.h
@@ -0,0 +1,18 @@
+//
+//  AutoLayouts.h
+//  AutoLayouts
+//
+//  Created by Icung on 30/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoLayouts.
+FOUNDATION_EXPORT double AutoLayoutsVersionNumber;
+
+//! Project version string for AutoLayouts.
+FOUNDATION_EXPORT const unsigned char AutoLayoutsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLayouts/PublicHeader.h>
+
+
