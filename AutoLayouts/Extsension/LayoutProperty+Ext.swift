0d81b8a18ae5f56bedff6cf2efc3817b96759533
icuuuuuuuuuung <saiful-islam@outlook.co.id>
2023-07-09 12:01:07 +0700

    add layout property (#7)

--- AutoLayouts/Extsension/LayoutProperty+Ext.swift
+++ AutoLayouts/Extsension/LayoutProperty+Ext.swift
@@ -11,4 +11,12 @@ public extension LayoutProperty {
     func equal(to otherAnchor: Anchor, offsetBy constant: CGFloat = 0) {
         anchor.constraint(equalTo: otherAnchor, constant: constant).isActive = true
     }
+    
+    func greaterThanOrEqual(to otherAnchor: Anchor, offsetBy constant: CGFloat = 0) {
+        anchor.constraint(greaterThanOrEqualTo: otherAnchor, constant: constant).isActive = true
+    }
+
+    func lessThanOrEqual(to otherAnchor: Anchor, offsetBy constant: CGFloat = 0) {
+        anchor.constraint(lessThanOrEqualTo: otherAnchor, constant: constant).isActive = true
+    }
 }
