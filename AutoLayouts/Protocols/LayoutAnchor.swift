908fac2668fb88531ded9c5c07237db6fc7675cd
icuuuuuuuuuung <saiful-islam@outlook.co.id>
2023-07-09 12:22:22 +0700

    Revert "add layout property (#7)"
    This reverts commit 0d81b8a18ae5f56bedff6cf2efc3817b96759533.

--- AutoLayouts/Protocols/LayoutAnchor.swift
+++ AutoLayouts/Protocols/LayoutAnchor.swift
@@ -9,6 +9,4 @@ import UIKit
 
 public protocol LayoutAnchor {
     func constraint(equalTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
-    func constraint(greaterThanOrEqualTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
-    func constraint(lessThanOrEqualTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
 }
